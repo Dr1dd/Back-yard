9ba7e711a9abf7b5f9c88a385a6d71277648c0e3
Dr1dd <43915140+Dr1dd@users.noreply.github.com>
2018-12-08 16:02:25 +0200

    Add files via upload

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+    apiKey: "AIzaSyCcVDgMUODnqnzY-5fVrlaesLJ2wsoDKs4",
+    authDomain: "nugarinis-jardas.firebaseapp.com",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+	}
+}
