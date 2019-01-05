324f9936b9aac566a7b7709ee839f286e9f6387d
Dr1dd <43915140+Dr1dd@users.noreply.github.com>
2019-01-05 22:21:38 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -13,8 +13,12 @@ import { addItem, addItem1} from './src/Services/ItemService';
 import Icon from 'react-native-vector-icons/Octicons';
 import Creating from './src/components/Screens-main/Creating';
 import Searching from './src/components/Screens-main/SearchScreen';
+import AdvertScreen from './src/components/Screens-main/AdvertScreen';
+import MyListings from './src/components/Screens-main/MyListings';
+import MyAds from './src/components/Screens-main/MyAds';
 import Logout from './src/components/Screens-main/Logout';
 
+//Pagrindinis js failiukas
 
 const MenuIcon = ({ navigate }) => <Icon
     name='three-bars'
@@ -22,38 +26,42 @@ const MenuIcon = ({ navigate }) => <Icon
     color='#000'
     onPress={() => navigate('DrawerOpen')}
 />;
-
-const Home = createStackNavigator(
+// Navigacija
+const Home = createStackNavigator( //Stack navigator t.y. paprasta navigacija tarp langų. Šiuo atveju tai yra pirmi programėlės langai:
   {
 
-    Loading,
-    SignUp,
-    Logges,
+    Loading, //1 Loading langas, kuris yra rodomas tik pajungus programėlę.
+    SignUp, // Registracijos langas, kuris eina po Loading lango.
+    Logges, // Prisijungimo langas
     },
     {
-     headerMode: 'none',
-      initialRouteName: 'Loading',
+     headerMode: 'none', // išjungiama antraštė
+      initialRouteName: 'Loading', // čia nustatomas pradinis langas
     }
  );
-const AppDrawerNavigator = createDrawerNavigator({
-    Profile: {
-    screen: Main,
+const AppDrawerNavigator = createDrawerNavigator({ // Kuriama Drawer navigator, t.y. navigacija, kurią galima pasiekti perbraukus pirštą (atsidaro kaip stalčius)
+
+    Profile: { // Profilio langas - pavadinimas
+    screen: Main, // Profilio klasės pavadinimas - Main
 
       navigationOptions: ({ navigation }) => ({
                 headerRight: MenuIcon(navigation)
             })
     },
-    Create: {screen: Creating},
-    Search: {screen: Searching},
-    Logout: {screen: Logout},
+    Create: {screen: Creating}, // Skelbimų kūrimo langas.
+    Search: {screen: Searching}, // Skelbimų langas
+    MyListings: {screen: MyListings}, // Mano skelbimai
+    Logout: {screen: Logout}, // Išėjimas iš programėlės
+    AdvertScreen, // langas, kuris atsiranda paspaudus ant skelbimo
+    MyAds, // langas, kuris atsiranda paspaudus ant savo skelbimo
 },
  {
- drawerBackgroundColor: '#E9F8F7',
+ drawerBackgroundColor: '#E9F8F7', // "stalčiaus' spalva
  headerMode: 'screen' }
 );
-const newRoute = StackNavigator({
-     home: {screen: Home},
-     Notifications: {screen: AppDrawerNavigator},
+const newRoute = StackNavigator({ // Nustatoma navigacijos eiga
+     home: {screen: Home}, // Pirma 'home' navigacija, t.y. Loading, Signup, Logges
+     Drawer: {screen: AppDrawerNavigator}, // Antra 'Drawer' navigacija
 },{headerMode:'none'}
 );
 export default newRoute;
