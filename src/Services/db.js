5ffce3cbbc10f136bae592f96c26ee47990b8bba
Laurynas Leišis <43915140+Dr1dd@users.noreply.github.com>
2020-02-06 15:39:22 +0200

    Update db.js

--- src/Services/db.js
+++ src/Services/db.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
  let config = {
-    apiKey: "AIzaSyCcVDgMUODnqnzY-5fVrlaesLJ2wsoDKs4",
-    authDomain: "https://nugarinis-jardas.firebaseapp.com",
-    databaseURL: "https://nugarinis-jardas.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "nugarinis-jardas",
     storageBucket: "gs://nugarinis-jardas.appspot.com",
-    messagingSenderId: "17867734620"
+    messagingSenderId: ""
   };
 let app = firebase.initializeApp(config);
 export const db = app.database();
